526e532445217c34aecaec939a910673dd086ef8
DimitarMilushev <DeadlyCookye@gmail.com>
2017-09-30 15:06:47 -0700

    Added C# Intro and Basic Syntax

---
+++ 02. Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = double.Parse(Console.ReadLine());
+            var number2 = double.Parse(Console.ReadLine());
+            var sum = number1 * number2;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
